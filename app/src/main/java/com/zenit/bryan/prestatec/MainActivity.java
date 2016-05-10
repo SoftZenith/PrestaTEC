dbd7a795742582a0c9c0a80d281bde1c67663cc9
Bryan Gutierrez <bhn_rock@hotmail.com>
2016-05-10 16:04:24 -0600

    Pantalla principal

--- app/src/main/java/com/zenit/bryan/prestatec/MainActivity.java
+++ app/src/main/java/com/zenit/bryan/prestatec/MainActivity.java
@@ -1,7 +1,11 @@
 package com.zenit.bryan.prestatec;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,64 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnPrestamos = (Button) findViewById(R.id.btnPrestamos);
+        Button btnContactos = (Button) findViewById(R.id.btnContactos);
+        Button btnConfiguracion = (Button) findViewById(R.id.btnConfiguracion);
+        Button btnAcerca = (Button) findViewById(R.id.btnAcerca);
+        Button btnSalir = (Button) findViewById(R.id.btnSalir);
+
+        btnPrestamos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarPrestamo();
+            }
+        });
+
+        btnContactos.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mostrarContactos();
+            }
+        });
+
+        btnConfiguracion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarConfiguracion();
+            }
+        });
+
+        btnAcerca.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alerta=new AlertDialog.Builder(MainActivity.this);
+                alerta.setMessage("Desarrollado por ZenitSoft\n" +
+                        "Brayan Jesus Gutierrez Esparza").setTitle("PrestaTEC").show();
+            }
+        });
+
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+
+    private void mostrarConfiguracion() {
+        Intent ventana = new Intent(this, Configuracion.class);
+        startActivity(ventana);
+    }
+
+    private void mostrarContactos() {
+        Intent ventana = new Intent(this,ContactoV.class);
+        startActivity(ventana);
+    }
+
+    private void mostrarPrestamo() {
+        Intent ventana = new Intent(this, vistaPrestamos.class);
+        startActivity(ventana);
     }
 }
---
+++ app/src/main/java/com/zenit/bryan/prestatec/vistaPrestamos.java
@@ -0,0 +1,13 @@
+package com.zenit.bryan.prestatec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class vistaPrestamos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_prestamos);
+    }
+}
